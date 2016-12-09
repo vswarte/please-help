f4ac1b53d0c6a638c8efd718614cfd3458ad3326
Niek den Breeje <n.j.den.breeje@gmail.com>
2016-12-09 13:19:56 +0000

    Formatting, added new lines to array object

--- help.go
+++ help.go
@@ -1,14 +1,20 @@
 package main
 
 import (
-        "fmt"
-        "math/rand"
-        "time"
+	"fmt"
+	"math/rand"
+	"time"
 )
 
-var lines = [4]string{"PLEASE HELP ME", "WHAT ARE YOU DOING", "I AM STUCK HERE", "OH GOD ITS HAPPENING AGAIN"}
+var lines = []string{
+	"PLEASE HELP ME",
+	"WHAT ARE YOU DOING",
+	"I AM STUCK HERE",
+	"OH GOD ITS HAPPENING AGAIN",
+	"MAKE IT STOP",
+	"I CAN'T TAKE IT ANYMORE"}
 
 func main() {
-        rand.Seed(time.Now().Unix())
-        fmt.Println(lines[rand.Int()%len(lines)])
+	rand.Seed(time.Now().Unix())
+	fmt.Println(lines[rand.Int()%len(lines)])
 }
